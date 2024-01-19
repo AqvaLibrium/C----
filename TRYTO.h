bb040dbd424feb9ed90db476ea01937748f58edd
AqvaLibrium <playerhdformat@gmail.com>
2024-01-19 04:05:23 +0300

    C++

---
+++ TRYTO.h
@@ -0,0 +1,123 @@
+#include "iostream"
+#include "cmath"
+#include "string"
+// 1 задача
+//int main()
+//{
+//    int number;
+//    int number2;
+//    int number3;
+//    std::cout << "input number ";
+//    std::cin >> number >> number2 >> number3;
+//    std::cout << (number + number2)  % number3;
+//}
+
+// 2 задача
+
+//int main(){
+//    int number;
+//    std::cin >> number;
+//    if (number % 2 == 0){
+//        std::cout << "EVEN";
+//    } else{
+//        std::cout << "ODD";
+//    }
+//    if (number > 0){
+//        std::cout << " POSITIVE";
+//    }else{
+//        std::cout << " NEGATIVE";
+//    }
+
+
+
+
+// 3 задача
+
+//int main(){
+//    int number;
+//    std::cin >> number;
+//    int numbers;
+//    int minNumber;
+//    std::cin >> minNumber;
+//
+//    for (int i = 1; i < number; i++){
+//        std::cin >> numbers;
+//
+//        if(minNumber > numbers){
+//            minNumber = numbers;
+//        }
+//
+//    }
+//    std::cout << minNumber;
+//}
+
+
+
+// 4 задача
+
+//int main(){
+//    int input_number;
+//    std::string s = "YES";
+//    std::cin >> input_number;
+//    if (input_number <= 1 || (input_number > 2 && input_number % 2 == 0)) {
+//        std::cout << "NO";
+//    }
+//    else{
+//        for (int i = 2; i * i <= input_number ;i++){
+//            if (input_number % i == 0){
+//                s = "NO";
+//                break;
+//            }
+//        }
+//        std::cout << s;
+//    }
+//
+//
+//}
+
+//  5 задача
+//
+//int main(){
+//    int number;
+//    int digit;
+//    int sum_dig = 0;
+//    std::cin >> number;
+//
+//    while(number > 0){
+//        digit = number % 10;
+//        sum_dig += digit;
+//        number /= 10;
+//
+//    }
+//    std::cout << sum_dig;
+//
+//}
+
+// 6 задача
+//int main(){
+//    int number1, number2;
+//    std::cin >> number1 >> number2;
+//
+//    char do_plus;
+//    std::cin >> do_plus;
+//
+//    switch (do_plus) {
+//        case '*':
+//            std::cout << number1 * number2 << std::endl;
+//            break;
+//        case '-':
+//            std::cout << number1 - number2 << std::endl;
+//            break;
+//        case '+':
+//            std::cout << number1 + number2 << std::endl;
+//            break;
+//        case '/':
+//            std::cout << number1 / number2 << std::endl;
+//
+//    }
+//
+//}
+
+
+
+
